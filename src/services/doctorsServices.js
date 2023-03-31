93c9a06e7c2aad09053db8e13d9d4f63f83c09b5
RubsRafa <rubiarafaela2000@gmail.com>
2023-03-31 16:11:48 -0300

    fix: searching for an doctor by name, location and specialty

--- src/services/doctorsServices.js
+++ src/services/doctorsServices.js
@@ -2,7 +2,7 @@ import errors from "../errors/index.js";
 import doctorsRepositories from "../repositories/doctorsRepositories.js"
 
 async function doctorsName({ name }) {
-    const { rowCount, rows: [doctor]} = await doctorsRepositories.getDoctorByName(name);
+    const { rowCount, rows: doctor} = await doctorsRepositories.getDoctorByName(name);
     if (!rowCount) throw errors.notFoundError('This doctor was not found');
 
     return doctor;
@@ -10,14 +10,14 @@ async function doctorsName({ name }) {
 
 
 async function doctorsSpecialty({ specialty }) {
-    const { rowCount, rows: [doctor] } = await doctorsRepositories.getDoctorBySpecialty(specialty);
+    const { rowCount, rows: doctor } = await doctorsRepositories.getDoctorBySpecialty(specialty);
     if (!rowCount) throw errors.notFoundError('No doctor found with this specialty');
 
     return doctor;
 }
 
 async function doctorsLocation({ idLocation }) {
-    const { rowCount, rows: [doctor] } = await doctorsRepositories.getDoctorByLocation(idLocation);
+    const { rowCount, rows: doctor } = await doctorsRepositories.getDoctorByLocation(idLocation);
     if(!rowCount) throw errors.notFoundError('No doctor found in this location')
 
     return doctor;
